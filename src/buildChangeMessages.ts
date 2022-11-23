6ada36ae0ce9d00e2db2c4c7558e21c143eac3c4
Grzegorz Pabian <gregoryppabian@yahoo.com>
2022-11-23 16:39:45 +0100

    INT-282 return whole files in changes (#2)
    Co-authored-by: Greg Pabian <grzpab@users.noreply.github.com>

--- src/buildChangeMessages.ts
+++ src/buildChangeMessages.ts
@@ -1,51 +1,21 @@
-import { diffChars } from 'diff';
 import { CodemodId } from './codemods';
 import { ChangeMessage, MessageKind } from './messages';
 
-const isTextRelevant = (text: string): boolean => {
-	return text.replace(/ /gm, '').replace(/\n/gm, '').length !== 0;
-};
-
-export const buildChangeMessages = (
+export const buildChangeMessage = (
 	filePath: string,
 	oldSource: string,
 	newSource: string,
 	codemodId: CodemodId,
-): ReadonlyArray<ChangeMessage> => {
-	const diffChanges = diffChars(oldSource, newSource);
-
-	const changes: ChangeMessage[] = [];
-	let leftCount = 0;
-
-	for (const diffChange of diffChanges) {
-		const count = diffChange.count ?? 0;
-
-		if (diffChange.added && isTextRelevant(diffChange.value)) {
-			const range: ChangeMessage['r'] = [leftCount, leftCount];
-
-			changes.push({
-				k: MessageKind.change,
-				p: filePath,
-				r: range,
-				t: diffChange.value,
-				c: codemodId,
-			});
-		} else if (diffChange.removed && isTextRelevant(diffChange.value)) {
-			const range: ChangeMessage['r'] = [leftCount, leftCount + count];
-
-			changes.push({
-				k: MessageKind.change,
-				p: filePath,
-				r: range,
-				t: diffChange.value,
-				c: codemodId,
-			});
-
-			leftCount += count;
-		} else {
-			leftCount += count;
-		}
+): ChangeMessage | null => {
+	if (oldSource === newSource) {
+		return null;
 	}
 
-	return changes;
+	return {
+		k: MessageKind.change,
+		p: filePath,
+		r: [0, oldSource.length],
+		t: newSource,
+		c: codemodId,
+	};
 };
