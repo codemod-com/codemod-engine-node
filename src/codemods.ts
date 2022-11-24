498c2d94f6ed171efa0a1b0d1c03e8a2fc5ec0a4
nikitamehrotra95 <nikita@intuita.io>
2022-11-24 14:21:30 +0530

    nextImageExperimental

--- src/codemods.ts
+++ src/codemods.ts
@@ -1,6 +1,8 @@
 import { nextJsNewLinkCodemod } from './cases/nextJsNewLink';
-import {nextJsMissingReactCodemod} from "./cases/addMissingReactImport"
+import {nextJsMissingReactCodemod} from "./cases/addMissingReactImport";
+import { nextImageExperimental } from './cases/nextImageExperimental';
 
-export const codemods = [nextJsNewLinkCodemod,nextJsMissingReactCodemod];
+
+export const codemods = [nextJsNewLinkCodemod,nextJsMissingReactCodemod,nextImageExperimental];
 
 export type CodemodId = typeof codemods[0]['id'];
