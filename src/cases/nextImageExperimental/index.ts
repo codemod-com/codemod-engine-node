5bda9f6eb2557636c4da1205c6a69fb6a1c892d9
nikitamehrotra95 <111195683+nikitamehrotra95@users.noreply.github.com>
2022-11-24 15:07:28 +0530

    Next image experimental2 (#7)
    * nextImageExperimental
    
    * add license and group property

---
+++ src/cases/nextImageExperimental/index.ts
@@ -0,0 +1,7 @@
+import { transformer } from "./transformer";
+
+export const nextImageExperimental = {
+	id: 'nextImageExperimental' as const,
+	group: 'nextJs' as const,
+	transformer,
+};
--- src/codemods.ts
+++ src/codemods.ts
@@ -1,6 +1,7 @@
 import { nextJsNewLinkCodemod } from './cases/nextJsNewLink';
 import { nextJsAddMissingReactImport } from './cases/nextJsAddMissingReactImport';
+import { nextImageExperimental } from './cases/nextImageExperimental';
 
-export const codemods = [nextJsNewLinkCodemod, nextJsAddMissingReactImport];
+export const codemods = [nextJsNewLinkCodemod,nextJsAddMissingReactImport,nextImageExperimental];
 
 export type CodemodId = typeof codemods[0]['id'];
