69d566a4d5fa6b0ed397abeeda97d5647762e0b9
Greg Pabian <grzpab@users.noreply.github.com>
2022-11-25 14:34:37 +0100

    fix

--- packages/nne-build-codemods/src/codemods.ts
+++ packages/nne-build-codemods/src/codemods.ts
@@ -15,4 +15,36 @@ export const codemods = [
 			'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/license.md',
 		withParser: 'tsx',
 	},
+	{
+		group: 'nextJs',
+		caseTitle: 'New Link',
+		url: 'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/transforms/new-link.ts',
+		license:
+			'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/license.md',
+		withParser: 'tsx',
+	},
+	{
+		group: 'nextJs',
+		caseTitle: 'Next Image Experimental',
+		url: 'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/transforms/next-image-experimental.ts',
+		license:
+			'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/license.md',
+		withParser: 'tsx',
+	},
+	{
+		group: 'nextJs',
+		caseTitle: 'Next Image to Legacy Image',
+		url: 'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/transforms/next-image-to-legacy-image.ts',
+		license:
+			'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/license.md',
+		withParser: 'tsx',
+	},
+	{
+		group: 'nextJs',
+		caseTitle: '',
+		url: 'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/transforms/url-to-withrouter.ts',
+		license:
+			'https://raw.githubusercontent.com/vercel/next.js/canary/packages/next-codemod/license.md',
+		withParser: 'tsx',
+	},
 ];
