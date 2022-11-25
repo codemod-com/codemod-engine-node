83191ef65b0e4e7469c9172a85fcc145db057712
Greg Pabian <grzpab@users.noreply.github.com>
2022-11-25 12:03:53 +0100

    pull codemods

--- packages/nne-codemods/src/index.ts
+++ packages/nne-codemods/src/index.ts
@@ -1,3 +1,24 @@
-console.log('TEST');
+import Axios from "axios";
+import { createHash } from "node:crypto";
+import { createWriteStream } from "node:fs";
+import { extname, join } from "node:path";
+import { codemods } from "./codemods";
 
-export const x = 1;
+const fetchCodemods = async () => {
+    // const index
+
+    for (const codemod of codemods) {
+        const response = await Axios.get(codemod.url, { responseType: 'stream' });
+
+        const hash = createHash('ripemd160').update(codemod.url).digest('base64url');
+        const extension = extname(codemod.url);
+
+        const filePath = join(__dirname, `../codemods/${hash}${extension}`);
+
+        response.data.pipe(createWriteStream(filePath));
+    }
+
+
+}
+
+fetchCodemods();
