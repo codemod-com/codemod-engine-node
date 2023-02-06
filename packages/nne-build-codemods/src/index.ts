49eea92dcf67c9be8e43d38d5355ca613161c0a3
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-06 15:38:23 +0100

    wip

--- packages/nne-build-codemods/src/index.ts
+++ packages/nne-build-codemods/src/index.ts
@@ -3,12 +3,20 @@ import { createReadStream, createWriteStream, existsSync } from 'node:fs';
 import { mkdir, readdir, readFile } from 'node:fs/promises';
 import { extname, join } from 'node:path';
 
-type CodemodObject = Readonly<{
-	group: string;
-	caseTitle: string;
-	transformer: string;
-	withParser: string;
-}>;
+type CodemodObject =
+	| Readonly<{
+			engine: 'jscodeshift';
+			group: string;
+			caseTitle: string;
+			transformer: string;
+			withParser: string;
+	  }>
+	| {
+			engine: 'filemod-engine';
+			group: string;
+			caseTitle: string;
+			transformerPath: string;
+	  };
 
 const dirname = join(__dirname, '../../nne-codemods/src/');
 
@@ -54,6 +62,42 @@ const fetchCodemods = async () => {
 				const hash = createHash('ripemd160')
 					.update(config.name)
 					.digest('hex');
+				const codemodDirname = join(dirname, `./codemods/${hash}/`);
+				if (!existsSync(codemodDirname)) {
+					await mkdir(codemodDirname);
+				}
+				{
+					const tsPath = join(codemodDirectoryPath, 'index.ts');
+					const jsPath = join(codemodDirectoryPath, 'index.js');
+					const path = existsSync(tsPath) ? tsPath : jsPath;
+					if (!existsSync(path)) {
+						throw new Error(`${path} does not exists`);
+					}
+					const ext = extname(path);
+					const readStream = createReadStream(path);
+					const filePath = join(codemodDirname, `index${ext}`);
+					if (!existsSync(filePath)) {
+						readStream.pipe(createWriteStream(filePath));
+					}
+				}
+				writeStream.write(
+					`import transformer${hash} from './codemods/${hash}';\n`,
+				);
+				codemodObjects.push({
+					engine: 'jscodeshift',
+					caseTitle: config.name,
+					group: setConfig.name,
+					transformer: `transformer${hash}`,
+					withParser: 'tsx',
+				});
+			}
+
+			if (config.engine === 'filemod-engine') {
+				console.log(config);
+
+				const hash = createHash('ripemd160')
+					.update(config.name)
+					.digest('hex');
 
 				const codemodDirname = join(dirname, `./codemods/${hash}/`);
 
@@ -61,44 +105,49 @@ const fetchCodemods = async () => {
 					await mkdir(codemodDirname);
 				}
 
-				{
-					const tsPath = join(codemodDirectoryPath, 'index.ts');
-					const jsPath = join(codemodDirectoryPath, 'index.js');
-
-					const path = existsSync(tsPath) ? tsPath : jsPath;
-
-					const ext = extname(path);
-
-					const readStream = createReadStream(path);
-					const filePath = join(codemodDirname, `index${ext}`);
-
-					if (!existsSync(filePath)) {
-						readStream.pipe(createWriteStream(filePath));
-					}
-				}
-
-				writeStream.write(
-					`import transformer${hash} from './codemods/${hash}';\n`,
+				const transformYmlPath = join(
+					codemodDirectoryPath,
+					'transform.yml',
 				);
 
+				if (!existsSync(transformYmlPath)) {
+					throw new Error(`${transformYmlPath} does not exists`);
+				}
+
+				const readStream = createReadStream(transformYmlPath);
+
+				const filePath = join(codemodDirname, `transform.yml`);
+
+				if (!existsSync(filePath)) {
+					readStream.pipe(createWriteStream(filePath));
+				}
+
 				codemodObjects.push({
+					engine: 'filemod-engine',
 					caseTitle: config.name,
 					group: setConfig.name,
-					transformer: `transformer${hash}`,
-					withParser: 'tsx',
+					transformerPath: `./${hash}/transform.yml`,
 				});
 			}
 		}
 	}
 
 	const stringifiedObjects = codemodObjects
-		.map(({ caseTitle, group, transformer, withParser }) => {
+		.map((codemodObject) => {
 			return (
 				'\t{\n' +
-				`\t\t"caseTitle": "${caseTitle}",\n` +
-				`\t\t"group": "${group}",\n` +
-				`\t\t"transformer": ${transformer},\n` +
-				`\t\t"withParser": "${withParser}",\n` +
+				`\t\t"engine": "${codemodObject.engine}",\n` +
+				`\t\t"caseTitle": "${codemodObject.caseTitle}",\n` +
+				`\t\t"group": "${codemodObject.group}",\n` +
+				('transformer' in codemodObject
+					? `\t\t"transformer": ${codemodObject.transformer},\n`
+					: '') +
+				('withParser' in codemodObject
+					? `\t\t"withParser": "${codemodObject.withParser}",\n`
+					: '') +
+				('transformerPath' in codemodObject
+					? `\t\t"transformerPath": "${codemodObject.transformerPath}",\n`
+					: '') +
 				'\t},\n'
 			);
 		})
