345e052c2133e15d8841f20aad2b7d91eade7141
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-06 19:44:28 +0100

    wip

---
+++ apps/nne/src/compositeModRunner.ts
@@ -0,0 +1,37 @@
+import { Codemod, runCodemod } from './codemodRunner';
+import { Filemod, runFilemod } from './filemodRunner';
+import { MessageKind } from './messages';
+
+export type CompositeMod = Readonly<{
+	engine: 'composite-mod-engine';
+	caseTitle: string;
+	group: string | null;
+	mods: ReadonlyArray<Filemod | Codemod>;
+}>;
+
+export const runCompositeMod = async (
+	compositeMod: CompositeMod,
+	filePath: string,
+) => {
+	const filePaths: string[] = [filePath];
+
+	for (const mod of compositeMod.mods) {
+		if (mod.engine === 'filemod-engine') {
+			for (const filePath of filePaths) {
+				const messages = await runFilemod(mod, filePath);
+
+				for (const message of messages) {
+					if (message.k === MessageKind.delete) {
+						message.oldFilePath;
+					}
+				}
+			}
+		}
+
+		if (mod.engine === 'jscodeshift') {
+			for (const filePath of filePaths) {
+				const messages = await runCodemod();
+			}
+		}
+	}
+};
