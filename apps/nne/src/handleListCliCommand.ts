edc3755d4c6a4aa81d1288cf21fd0d0492ad7238
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-04-24 19:25:05 +0200

    INT-799 return the list of codemods from the codemod-engine-node

---
+++ apps/nne/src/handleListCliCommand.ts
@@ -0,0 +1,40 @@
+import { codemods as nneCodemods } from '@nne/codemods';
+import { codemods as muiCodemods } from '@nne/mui-codemods';
+import { createHash } from 'node:crypto';
+
+type CodemodEntry = Readonly<{
+	kind: 'codemod';
+	hashDigest: string;
+	name: string;
+}>;
+
+export const handleListCliCommand = () => {
+	const entries: CodemodEntry[] = [];
+
+	for (const codemod of nneCodemods) {
+		const hashDigest = createHash('ripemd160')
+			.update(codemod.caseTitle)
+			.digest('base64url');
+
+		entries.push({
+			kind: 'codemod',
+			hashDigest,
+			name: codemod.caseTitle,
+		});
+	}
+
+	// TODO: this for loop will be soon removed
+	for (const codemod of muiCodemods) {
+		const hashDigest = createHash('ripemd160')
+			.update(codemod.caseTitle)
+			.digest('base64url');
+
+		entries.push({
+			kind: 'codemod',
+			hashDigest,
+			name: codemod.caseTitle,
+		});
+	}
+
+	console.log(JSON.stringify(entries));
+};
