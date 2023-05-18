46078e56893b65ffd55542ffaf25ae698d99e209
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-05-18 16:20:52 +0200

    wip

--- apps/nne/src/handleListCliCommand.ts
+++ apps/nne/src/handleListCliCommand.ts
@@ -4,6 +4,7 @@ import { createHash } from 'node:crypto';
 
 type CodemodEntry = Readonly<{
 	kind: 'codemod';
+	engine: 'jscodeshift' | 'ts-morph' | 'repomod-engine';
 	hashDigest: string;
 	name: string;
 	description: string;
@@ -19,6 +20,7 @@ export const handleListCliCommand = () => {
 
 		entries.push({
 			kind: 'codemod',
+			engine: codemod.engine as CodemodEntry['engine'],
 			hashDigest,
 			name: codemod.caseTitle,
 			description:
@@ -34,9 +36,26 @@ export const handleListCliCommand = () => {
 
 		entries.push({
 			kind: 'codemod',
+			engine: 'jscodeshift',
 			hashDigest,
 			name: codemod.caseTitle,
 			description: '',
+		});
+	}
+
+	{
+		// TODO hack
+		const hashDigest = createHash('ripemd160')
+			.update('next/13/app-directory-boilerplate')
+			.digest('base64url');
+
+		entries.push({
+			kind: 'codemod',
+			engine: 'repomod-engine',
+			hashDigest,
+			name: 'next/13/app-directory-boilerplate',
+			description:
+				'This codemod provides boilerplate for the app directory.',
 		});
 	}
 
