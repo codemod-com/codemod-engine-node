39d976e53fda8753840230a445d5e7f4e11fffa2
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-11-02 13:19:05 +0100

    fix

--- src/workerThreadManager.ts
+++ src/workerThreadManager.ts
@@ -36,10 +36,7 @@ export class WorkerThreadManager {
 			this.__idleWorkerIds.push(i);
 			this.__workerTimestamps.push(Date.now());
 
-			const filename =
-				typeof global.__filename === 'string'
-					? global.__filename
-					: './src/index.ts';
+			const filename = process.env.TEST ? './src/index.ts' : __filename;
 
 			const worker = new Worker(filename);
 
