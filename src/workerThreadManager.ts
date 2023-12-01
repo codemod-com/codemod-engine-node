1975b978da4fdf852286204816c09b3844bbde10
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-12-01 09:43:07 +0100

    INT-2191 allow to close the surface-agnostic case on demand (#156)

--- src/workerThreadManager.ts
+++ src/workerThreadManager.ts
@@ -7,7 +7,6 @@ import {
 } from './workerThreadMessages.js';
 import { FormattedFileCommand } from './fileCommands.js';
 import { SafeArgumentRecord } from './safeArgumentRecord.js';
-import { CodemodEngine } from './codemod.js';
 
 const WORKER_THREAD_TIME_LIMIT = 10000;
 
@@ -25,10 +24,7 @@ export class WorkerThreadManager {
 	public constructor(
 		private readonly __workerCount: number,
 		private readonly __codemodPath: string,
-		private readonly __codemodEngine: Extract<
-			CodemodEngine,
-			'ts-morph' | 'jscodeshift'
-		>,
+		private readonly __codemodEngine: 'ts-morph' | 'jscodeshift',
 		private readonly __codemodSource: string,
 		private readonly __formatWithPrettier: boolean,
 		private readonly __getData: (path: string) => Promise<string>,
