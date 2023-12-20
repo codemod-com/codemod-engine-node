91eb2eaaceb07f4c264650fc0a391b742368cb53
Benny Joo <sldisek783@gmail.com>
2023-12-20 11:57:48 +0000

    make openURL a utill

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,29 @@
+import { spawnSync } from 'node:child_process';
+
 export const doubleQuotify = (str: string): string =>
 	str.startsWith('"') && str.endsWith('"') ? str : `"${str}"`;
+
+export const openURL = (url: string): boolean => {
+	// `spawnSync` is used because `execSync` has an input length limit
+	let command;
+	let args;
+
+	if (process.platform === 'win32') {
+		command = 'start';
+		args = [url];
+	} else {
+		command = 'open';
+		args = [url];
+	}
+	// By setting `shell: false`,
+	// we avoid potential command-line length limitations
+	// and the full URL should be passed to the default web browser without getting truncated
+
+	try {
+		spawnSync(command, args, { stdio: 'ignore', shell: false });
+		return true;
+	} catch (error) {
+		console.error('Error while opening URL:', error);
+		return false;
+	}
+};
