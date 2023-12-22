e4bd7c762e1ccc96af081d21f671a37cb0e8e6cf
Sasha Mysak <reactionnnnn@gmail.com>
2023-12-22 14:44:11 +0100

    trycatch

--- src/handleLogoutCliCommand.ts
+++ src/handleLogoutCliCommand.ts
@@ -1,25 +1,31 @@
-import { existsSync } from 'node:fs';
 import { readFile, unlink } from 'node:fs/promises';
 import { homedir } from 'node:os';
 import { join } from 'node:path';
-import type { PrinterBlueprint } from './printer.js';
 import { revokeCLIToken } from './apis.js';
+import type { PrinterBlueprint } from './printer.js';
 
 export const handleLogoutCliCommand = async (printer: PrinterBlueprint) => {
 	const tokenTxtPath = join(homedir(), '.intuita', 'token.txt');
 
-	if (!existsSync(tokenTxtPath)) {
+	let token: string | null = null;
+	try {
+		token = await readFile(tokenTxtPath, 'utf-8');
+	} catch (err) {
 		printer.printConsoleMessage('info', 'You are already logged out.');
 		return;
 	}
 
-	try {
-		const token = await readFile(tokenTxtPath, 'utf-8');
-		await revokeCLIToken(token.trim());
-	} catch (error) {
-		// Don't inform user if something went wrong, just delete the token file.
-	}
+	if (token !== null) {
+		try {
+			await revokeCLIToken(token.trim());
+		} catch (err) {
+			// Don't inform user if something went wrong, just delete the token file.
+		}
 
-	await unlink(tokenTxtPath);
-	printer.printConsoleMessage('info', 'You have successfully logged out.');
+		await unlink(tokenTxtPath);
+		printer.printConsoleMessage(
+			'info',
+			'You have successfully logged out.',
+		);
+	}
 };
